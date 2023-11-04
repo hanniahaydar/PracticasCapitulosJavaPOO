42a0de44fef4a64b7dd55ede8c8618047fe6dcd1
hanniahaydar <148922406+hanniahaydar@users.noreply.github.com>
2023-11-03 20:17:33 -0600

    añadí código

--- PracticasLibro/src/Capitulo13/Car.java
+++ PracticasLibro/src/Capitulo13/Car.java
@@ -5,3 +5,18 @@ public class Car {
 	 private String make; 
 	 private int year; 
 	 private String color; 
+	 
+	 public Car(String make, int year, String color)
+	 {
+	 this.make = make;
+	 this.year = year;
+	 this.color = color;
+	 }
+	 public boolean equals(Car otherCar)
+	 {
+	 return otherCar != null &&
+	 make.equals(otherCar.make) &&
+	 year == otherCar.year &&
+	 color.equals(otherCar.color);
+	 } 
+}
