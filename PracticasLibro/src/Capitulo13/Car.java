27b29e5307e249d7b9b9e5597c93f06e5aa4908c
hanniahaydar <148922406+hanniahaydar@users.noreply.github.com>
2023-11-03 20:16:19 -0600

    .

--- PracticasLibro/src/Capitulo13/Car.java
+++ PracticasLibro/src/Capitulo13/Car.java
@@ -1,31 +1,7 @@
 package Capitulo13;
 
 public class Car {
-	************************************************
-	* Car.java
-	* Dean & Dean
-	*
-	* Esto define y compara automóviles.
-	************************************************/
-	public class Car
-	{
-	 private String make; // modelo del automóvil
-	 private int year; // año de lista del automóvil
-	 private String color; // color del automóvil
-	 //*********************************************
-	 public Car(String make, int year, String color)
-	 {
-	 this.make = make;
-	 this.year = year;
-	 this.color = color;
-	 } // end Car constructor
-	 //*********************************************
-	 public boolean equals(Car otherCar)
-	 {
-	 return otherCar != null &&
-	 make.equals(otherCar.make) &&
-	 year == otherCar.year &&
-	 color.equals(otherCar.color);
-	 } // end equals
-	} // end class Car
-}
+
+	 private String make; 
+	 private int year; 
+	 private String color; 
